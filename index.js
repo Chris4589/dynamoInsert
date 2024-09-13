0ad7daf48b1bb7aebca0bc0a7610c94df83ec116
クリス <58378442+Chris4589@users.noreply.github.com>
2024-09-13 08:36:29 -0600

    Update index.js

--- index.js
+++ index.js
@@ -37,13 +37,38 @@ const insertData = () => {
                 } else if (typeof item.constantCode === 'string') {
                     putRequest.Key.constantCode = item.constantCode.toString();
                 }
-            }
-
-            if (item.code) {
-                if (typeof item.code === 'number') {
-                    putRequest.Key.code = item.code;
-                } else if (typeof item.code === 'string') {
-                    putRequest.Key.code = item.code.toString();
+                if (item.code) {
+                    if (typeof item.code === 'number') {
+                        putRequest.Key.code = item.code;
+                    } else if (typeof item.code === 'string') {
+                        putRequest.Key.code = item.code.toString();
+                    }
+                }
+            } else if (item.orderCode) {
+                if (item.code) {
+                    if (typeof item.code === 'number') {
+                        putRequest.Key.code = item.code;
+                    } else if (typeof item.code === 'string') {
+                        putRequest.Key.code = item.code.toString();
+                    }
+                }
+                if (typeof item.orderCode === 'number') {
+                    putRequest.Key.orderCode = item.orderCode;
+                } else if (typeof item.orderCode === 'string') {
+                    putRequest.Key.orderCode = item.orderCode.toString();
+                }
+            } else if (item.stateCode) {
+                if (typeof item.stateCode === 'number') {
+                    putRequest.Key.stateCode = item.stateCode;
+                } else if (typeof item.stateCode === 'string') {
+                    putRequest.Key.stateCode = item.stateCode.toString();
+                }
+                if (item.code) {
+                    if (typeof item.code === 'number') {
+                        putRequest.Key.code = item.code;
+                    } else if (typeof item.code === 'string') {
+                        putRequest.Key.code = item.code.toString();
+                    }
                 }
             }
 
